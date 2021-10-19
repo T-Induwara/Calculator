264d6643d897cf4f2eae1508a825ee4743d298d1
Tharusha Induwara <tharusha.induwar@gmail.com>
2021-10-19 13:11:57 +0530

    Added numbers display on input field in JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 console.log("Script loaded");
 
+var display = document.getElementById("number-input");
+
 var num0 = document.getElementById("0");
 var num1 = document.getElementById("1");
 var num2 = document.getElementById("2");
@@ -10,3 +12,45 @@ var num6 = document.getElementById("6");
 var num7 = document.getElementById("7");
 var num8 = document.getElementById("8");
 var num9 = document.getElementById("9");
+
+var minus = document.getElementById("-");
+var add = document.getElementById("+");
+var devide = document.getElementById("/");
+var multi = document.getElementById("*");
+var equal = document.getElementById("=");
+var clear = document.getElementById("c");
+
+num0.onclick = function(input){
+    display.value +='0';
+}
+num1.onclick = function(input){
+    display.value +='1';
+}
+num2.onclick = function(input){
+    display.value +='2';
+}
+num3.onclick = function(input){
+    display.value +='3';
+}
+num4.onclick = function(input){
+    display.value +='4';
+}
+num5.onclick = function(input){
+    display.value +='5';
+}
+num6.onclick = function(input){
+    display.value +='6';
+}
+num7.onclick = function(input){
+    display.value +='7';
+}
+num8.onclick = function(input){
+    display.value +='8';
+}
+num9.onclick = function(input){
+    display.value +='9';
+}
+clear.onclick = function(input){
+    display.value='';
+}
+
