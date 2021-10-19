67f3dc646d0189b8c50d89febefde0165bcc557f
Tharusha Induwara <tharusha.induwar@gmail.com>
2021-10-19 11:48:22 +0530

    Introduced Js variables

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log("Script loaded");
+
+var num0 = document.getElementById("0");
+var num1 = document.getElementById("1");
+var num2 = document.getElementById("2");
+var num3 = document.getElementById("3");
+var num4 = document.getElementById("4");
+var num5 = document.getElementById("5");
+var num6 = document.getElementById("6");
+var num7 = document.getElementById("7");
+var num8 = document.getElementById("8");
+var num9 = document.getElementById("9");
